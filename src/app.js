2d8c7efd62e26a388072a91ca05754f7848bd1d5
Marlon Nhantumbo <128413802+Marlon200530@users.noreply.github.com>
2024-09-23 04:35:15 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 require('dotenv').config();
 require("./db/mongoose.js"); //conetar a base dados
 const user = require('./routers/users.js');
+const cors = require('cors');
 const exam = require('./routers/exames.js');
 const auth = require('./middleware/auth.js');
 const app = express();
@@ -9,6 +10,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use(user);
 app.use(exam);
 
